5bcc92cacd711ca8052d60f2b40987226ee50224
Dennis Paagman <dennispaagman@gmail.com>
2017-06-15 12:15:07 +0200

    Use chrome.tabs.duplicate API so history etc is preserved

--- tab-duplicator.js
+++ tab-duplicator.js
@@ -10,6 +10,6 @@ function duplicateTab() {
   chrome.tabs.query({ currentWindow: true }, function(tabs) {
     active_tab = tabs.find(function(t) { return t.active == true });
 
-    chrome.tabs.create({ url: active_tab.url, index: active_tab.index + 1 });
+    chrome.tabs.duplicate(active_tab.id);
   });
 }
